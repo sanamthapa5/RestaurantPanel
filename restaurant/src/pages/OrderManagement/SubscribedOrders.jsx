858905cc30c53c5b946a3a2542a73ed354d6eb3b
Sanam <Sanamthapa112233@gmail.com>
2025-05-04 19:49:10 +0545

    Subscribed Orders Fixed

--- restaurant/src/pages/OrderManagement/SubscribedOrders.jsx
+++ restaurant/src/pages/OrderManagement/SubscribedOrders.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import "./SubscribedOrders.css";
-import { Search, Info, Eye } from "lucide-react";
+import { Search, Info, Eye, CalendarCheck } from "lucide-react";
 
 const SubscribedOrders = () => {
   const [orders, setOrders] = useState([
@@ -84,47 +84,12 @@ const SubscribedOrders = () => {
     <div className="subscribed-orders-container">
       <div className="Subscribed-Header">
         <div className="title-section">
-          <div className="icon-container">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M16 2V6"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M8 2V6"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M3 10H21"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </div>
-          <h1>
-            Subscribed Orders <span className="order-count">5</span>
-          </h1>
+          <span className="Subscribed-title-section">
+            <CalendarCheck />
+          </span>
+          <h1>Subscribed Orders</h1>
+
+          <span className="order-count">{orders.length}</span>
         </div>
         <div className="info-section">
           <button className="info-button">

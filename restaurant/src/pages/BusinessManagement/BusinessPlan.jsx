a9626b3ee79c310d0b6ff746e4749a256a5b0e4f
Sanam <Sanamthapa112233@gmail.com>
2025-05-06 21:36:17 +0545

    Fixed Modal on BusinessPlan

--- restaurant/src/pages/BusinessManagement/BusinessPlan.jsx
+++ restaurant/src/pages/BusinessManagement/BusinessPlan.jsx
@@ -2,12 +2,10 @@
 
 import { useState } from "react";
 import "./BusinessPlan.css";
-import { Store, Check } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { Store, Check, X } from "lucide-react";
 
 const BusinessPlan = () => {
   const [showModal, setShowModal] = useState(false);
-  const navigate = useNavigate();
 
   const toggleModal = () => {
     setShowModal(!showModal);
@@ -20,26 +18,62 @@ const BusinessPlan = () => {
       "Restaurant will pay 10% Commission to StackFood From each order. You will get access of all the features and options in restaurant panel, app and interaction with user.",
   };
 
-  // const handleDetailsClick = () => {
-  //   navigate("/business-plan/details", { state: { plan: currentPlan } });
-  // };
+  const otherPlans = [
+    {
+      name: "Pro",
+      price: "$1,199.00",
+      duration: "365 Days",
+      features: [
+        "POS",
+        "Mobile app",
+        "Chatting options",
+        "Review section",
+        "Self delivery",
+      ],
+    },
+    {
+      name: "Standard",
+      price: "$799.00",
+      duration: "180 Days",
+      features: [
+        "POS",
+        "Mobile app",
+        "Chatting options",
+        "Review section",
+        "Unlimited Orders",
+      ],
+    },
+    {
+      name: "Basic",
+      price: "$399.00",
+      duration: "120 Days",
+      features: [
+        "POS",
+        "Mobile app",
+        "Review section",
+        "400 Orders",
+        "30 Uploads",
+      ],
+    },
+  ];
 
   return (
-    <div className="BusinessPlan-business-plan-container">
-      <header className="BusinessPlan-header">
-        <div className="BusinessPlan-logo-containerPlan">
-          <Store className="BusinessPlan-store-icon" />
-          <h1>Hungry Puppets Business Plan</h1>
+    <div className="BusinessPlan-withdraw-container">
+      <div className="BusinessPlan-withdraw-header">
+        <div className="BusinessPlan-withdraw-title">
+          <span className="BusinessPlan-icon-person">
+            <Store />
+          </span>
+          <h1>Business Plan Setup</h1>
         </div>
-      </header>
-
-      <div className="BusinessPlan-content">
-        <div className="BusinessPlan-overview-section">
-          <div className="BusinessPlan-overview-header">
-            <span className="BusinessPlan-overview-icon">📊</span>
-            <span>Overview</span>
+      </div>
+      <div className="BusinessPlan-withdraw-box">
+        <div className="BusinessPlan-withdraw-content">
+          <div className="BusinessPlan-withdraw-methods-header">
+            <div className="BusinessPlan-methods-title">
+              <h2>Business Plans</h2>
+            </div>
           </div>
-
           <div className="BusinessPlan-plan-card">
             <h2 className="BusinessPlan-plan-title">{currentPlan.name}</h2>
             <div className="BusinessPlan-commission-rate">
@@ -53,14 +87,14 @@ const BusinessPlan = () => {
             <p className="BusinessPlan-plan-description">
               {currentPlan.description}
             </p>
-            <div className="BusinessPlan-plan-buttons">
-              <button
-                className="BusinessPlan-change-plan-btn"
-                onClick={toggleModal}
-              >
-                Change Business Plan
-              </button>
-            </div>
+            {/* <div className="BusinessPlan-plan-buttons"> */}
+            <button
+              className="BusinessPlan-change-plan-btn"
+              onClick={toggleModal}
+            >
+              Change Business Plan
+            </button>
+            {/* </div> */}
           </div>
         </div>
       </div>
@@ -68,6 +102,17 @@ const BusinessPlan = () => {
       {showModal && (
         <div className="BusinessPlan-modal-overlay">
           <div className="BusinessPlan-modal-content">
+            <button
+              className="BusinessPlan-modal-close-btn"
+              onClick={toggleModal}
+            >
+              <X />
+            </button>
+            <div className="BusinessPlan-modal-header-text">
+              <h2>Choose a Business</h2>
+
+              <p>Choose a Business plan to get better experience!</p>
+            </div>
             <div className="BusinessPlan-plans-container">
               <div className="BusinessPlan-current-plan">
                 <h3>Commission Base</h3>
@@ -82,86 +127,25 @@ const BusinessPlan = () => {
                 </button>
               </div>
 
-              <div className="BusinessPlan-plan-option">
-                <h3>Pro</h3>
-                <div className="BusinessPlan-plan-price">$ 1,199.00</div>
-                <div className="BusinessPlan-plan-duration">365 Days</div>
-                <ul className="BusinessPlan-features-list">
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> POS
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Mobile app
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Chatting
-                    options
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Review section
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Self delivery
-                  </li>
-                </ul>
-                <button className="BusinessPlan-shift-plan-btn">
-                  Shift In This Plan
-                </button>
-              </div>
-
-              <div className="BusinessPlan-plan-option">
-                <h3>Standard</h3>
-                <div className="BusinessPlan-plan-price">$ 799.00</div>
-                <div className="BusinessPlan-plan-duration">180 Days</div>
-                <ul className="BusinessPlan-features-list">
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> POS
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Mobile app
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Chatting
-                    options
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Review section
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Unlimited
-                    Orders
-                  </li>
-                </ul>
-                <button className="BusinessPlan-shift-plan-btn">
-                  Shift In This Plan
-                </button>
-              </div>
-
-              <div className="BusinessPlan-plan-option">
-                <h3>Basic</h3>
-                <div className="BusinessPlan-plan-price">$ 399.00</div>
-                <div className="BusinessPlan-plan-duration">120 Days</div>
-                <ul className="BusinessPlan-features-list">
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> POS
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Mobile app
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> Review section
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> 400 Orders
-                  </li>
-                  <li>
-                    <Check className="BusinessPlan-check-icon" /> 30 Uploads
-                  </li>
-                </ul>
-                <button className="BusinessPlan-shift-plan-btn">
-                  Shift In This Plan
-                </button>
-              </div>
+              {otherPlans.map((plan, index) => (
+                <div key={index} className="BusinessPlan-plan-option">
+                  <h3>{plan.name}</h3>
+                  <div className="BusinessPlan-plan-price">{plan.price}</div>
+                  <div className="BusinessPlan-plan-duration">
+                    {plan.duration}
+                  </div>
+                  <ul className="BusinessPlan-features-list">
+                    {plan.features.map((feature, idx) => (
+                      <li key={idx}>
+                        <Check className="BusinessPlan-check-icon" /> {feature}
+                      </li>
+                    ))}
+                  </ul>
+                  <button className="BusinessPlan-shift-plan-btn">
+                    Shift In This Plan
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
         </div>
